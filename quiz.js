380bb4d106c862f407641285547069bf6001d26a
Akhil-Movva <akhilmovva@gmail.com>
2021-01-05 21:14:43 +0530

    added quiz section.

---
+++ quiz.js
@@ -0,0 +1,28 @@
+var ansOne=document.querySelector("#sixty");
+var form=document.querySelector("#form");
+
+
+
+function clickHandler(event){
+    event.preventDefault();
+    var score=0;
+    if(document.getElementById("sixty").checked==true)
+      score++;
+    
+    if(document.getElementById("obtuse").checked==true)
+      score++;
+
+    if(document.getElementById("acute").checked==true)
+      score++;
+    
+      if(document.getElementById("right").checked==true)
+      score++;
+      
+      console.log(score);
+
+      var message="Your score: "+score.toString();
+      document.getElementById("score").innerText=message;
+
+}
+
+form.addEventListener("submit",clickHandler);
